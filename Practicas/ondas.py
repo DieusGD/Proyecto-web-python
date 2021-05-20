7b67daa05142453f00e889191cacc49a6200d148
Dieus6D <cuchofeick@hotmail.com>
2021-05-20 14:13:20 -0500

    purbea de dasodasdasd

---
+++ Practicas/ondas.py
@@ -0,0 +1,14 @@
+#busco analizar como resolver un problema de ondas
+
+velSonido = 340
+velObs = 4
+velFuente = 20
+Frecuencia = 1000
+
+temp = velSonido - velObs
+temp2 = velSonido - velFuente
+temp3 = temp/temp2
+frecFin = Frecuencia*temp3
+print(frecFin)2
+
+#pruevba de cambviosd en pagina 
